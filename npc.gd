bd46975cbe0a98c652147336b555e0a11decbfa1
Fishlung <bigbadbug999@gmail.com>
2026-05-11 21:28:21 -0700

    Moving changes from personal project to this one

--- npc.gd
+++ npc.gd
@@ -1,10 +1,8 @@
 extends CharacterBody3D
+class_name RoamingMonster
 
-# ================== BEÁLLÍTÁSOK ==================
-
-@export var max_health:int = 10
-@export var current_health:int
-
+# ================== SETTINGS ==================
+@export_group("Settings")
 @export var max_chase_distance := 50.0
 
 @export var turn_speed := 6.0
@@ -12,74 +10,55 @@ extends CharacterBody3D
 @export var acceleration := 8.0
 @export var deceleration := 10.0
 
-@export var chase_strafe_strength := 0.6   # mennyire tér ki oldalra
+@export var chase_strafe_strength := 0.2   # How much does it move side to side when chasing?
 @export var chase_strafe_change_time := 1.2
 
-var current_speed := 0.0
-
-var strafe_dir := 0.0
-var strafe_timer := 0.0
-
-@export var speed := 3.2
-@export var chase_speed := 4.6
+@export var speed := 4.6
 @export var gravity := 20.0
 
-@export var detection_range := 14.0
-@export var lose_range := 18.0
 @export var follow_distance := 1.5
 
 @export var wander_min_time := 2.0
 @export var wander_max_time := 5.0
 
-# ================== NODE REFERENCIÁK ==================
+# ================== NODE REFERENCES ==================
 
-@export var ground_ray: RayCast3D
+@export_group("Node References")
 @export var front_ray: RayCast3D
 @export var left_ray: RayCast3D
 @export var right_ray: RayCast3D
+@export var line_of_sight: RayCast3D
+@export var astar_map: Node
 
-# ================== ÁLLAPOTOK ==================
+# ================== STATES ==================
 
 enum State {
 	IDLE,
-	WANDER,
-	CHASE
+	CHASE, 
 }
 
-var state: State = State.WANDER
+var state: State = State.IDLE
 var target: Node3D
+var target_closest_node: NavigationNode
+var current_goal: Node3D
+var goal_index: int = 0
+var target_path: Array
+var current_speed := 0.0
+var strafe_dir := 0.0
+var strafe_timer := 0.0
 
 # ================== WANDER ==================
 
 var wander_dir: Vector3 = Vector3.ZERO
 var wander_time := 0.0
+var idle_timer: float = 0
 
 # ================== READY ==================
 
 func _ready():
 	randomize()
-	set_target(get_tree().get_first_node_in_group("player"))
+	set_target(get_tree().get_first_node_in_group("Players"))
 	_reset_strafe()
-	_pick_new_wander()
-	
-	current_health = max_health
-	
-	$Health3D/SubViewport/HPControl/HPBar.max_value = max_health
-	$Health3D/SubViewport/HPControl/HPBar.value = current_health
-	
-func _get_damage(damage:int):
-	current_health -= damage
-	
-	$Health3D/SubViewport/HPControl/HPBar.max_value = max_health
-	$Health3D/SubViewport/HPControl/HPBar.value = current_health
-	$Health3D/SubViewport/HPControl/Label.text = str(current_health) + " / " + str(max_health)
-	#$Health3D/AnimationPlayer.play("in")
-	
-	if current_health <= 0:
-		_die()
-
-func _die():
-	queue_free()
 
 func _reset_strafe():
 	strafe_timer = randf_range(0.6, chase_strafe_change_time)
@@ -91,69 +70,51 @@ func _update_speed(target_speed: float, delta: float):
 	else:
 		current_speed = move_toward(current_speed, target_speed, deceleration * delta)
 
-# ================== MAIN LOOP ==================
+# ================== Process ==================
 func _physics_process(delta):
 	_apply_gravity(delta)
 
 	match state:
 		State.IDLE:
 			_process_idle(delta)
-		State.WANDER:
-			_process_wander(delta)
 		State.CHASE:
 			_process_chase(delta)
 
 	move_and_slide()
 
-# ================== ÁLLAPOT LOGIKA ==================
+# ================== STATE LOGIC ==================
 
 func _process_idle(delta):
-	velocity.x = 0
-	velocity.z = 0
-
-	_check_player_detection()
-
-func _process_wander(delta):
-	wander_time -= delta
-
-	if wander_time <= 0:
-		_pick_new_wander()
-
-	if _is_path_blocked() or not ground_ray.is_colliding():
-		_pick_new_wander(true)
-
-	_update_speed(speed, delta)
-
-	velocity.x = wander_dir.x * current_speed
-	velocity.z = wander_dir.z * current_speed
-
-	_smooth_look(wander_dir, delta)
-	_check_player_detection()
-
+	idle_timer += delta
+	if idle_timer >= 5:
+		state = State.CHASE
 
 func _process_chase(delta):
-	if not target:
-		state = State.WANDER
+	if !target:
+		idle_timer = 10
+		state = State.IDLE
 		return
+	move_to_target(delta)
 
-	var to_target = target.global_position - global_position
-	to_target.y = 0
+# ================== AUXILLARY FUNCTIONS==================
 
-	var distance = to_target.length()
+func move_to_target(delta):
+	calculate_target_path()
 	
-	# --------- OPTIMALIZÁLT TÁVOLSÁG ELLENŐRZÉS ---------
-	if distance > max_chase_distance:
-		state = State.WANDER
-		current_speed = 0.0
-		return
-	elif distance > lose_range:
-		state = State.WANDER
-		_update_speed(0, delta)
-		return
+	var to_goal = current_goal.global_position - global_position
+	to_goal.y = 0
 
-	var dir = to_target.normalized()
+	var distance = to_goal.length()
+	
+	# --------- OPTIMIZED DISTANCE CONTROL ---------
+	if distance <= 1: # Consider the goal reached if you get close enough
+		goal_index += 1
+		if target_path.size() > goal_index:
+			current_goal = instance_from_id(target_path[goal_index])
 
-	# --------- STRAFE LOGIKA ---------
+	var dir = to_goal.normalized()
+
+	# --------- STRAFE LOGIC ---------
 	strafe_timer -= delta
 	if strafe_timer <= 0:
 		_reset_strafe()
@@ -162,15 +123,20 @@ func _process_chase(delta):
 	dir += right * strafe_dir * chase_strafe_strength
 	dir = dir.normalized()
 
-	# --------- AKADÁLYKERÜLÉS ---------
-	if _is_path_blocked():
-		dir = _avoid_direction(dir)
-
-	# --------- SEBESSÉG ---------
-	var target_speed = chase_speed
-	if distance < follow_distance:
+	# --------- SPEED ---------
+	var target_speed = speed
+	if distance < follow_distance && (
+			goal_index >= target_path.size() - 1  # Ensures NPC actually reaches current goal
+			|| current_goal == target): # But slow down around the targeted player
 		target_speed = 0.0
 
+	# --------- OBSTACLE AVOIDANCE ---------
+	
+	if _is_path_blocked():
+		target_speed *= 1.6 # Try to push through blockages
+		dir = _avoid_direction(dir)
+	
+	
 	_update_speed(target_speed, delta)
 
 	velocity.x = dir.x * current_speed
@@ -178,15 +144,13 @@ func _process_chase(delta):
 
 	_smooth_look(dir, delta)
 
-# ================== SEGÉD FUNKCIÓK ==================
-
 func _apply_gravity(delta):
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 	else:
 		velocity.y = 0
 
-func _pick_new_wander(force := false):
+func _pick_new_wander(_force := false):
 	wander_time = randf_range(wander_min_time, wander_max_time)
 
 	var dir = Vector3(
@@ -197,43 +161,90 @@ func _pick_new_wander(force := false):
 
 	wander_dir = dir
 
-func _check_player_detection():
-	if not target:
-		return
-
-	var dist = global_position.distance_to(target.global_position)
-	if dist <= detection_range and dist <= max_chase_distance:
-		state = State.CHASE
-
 func _is_path_blocked() -> bool:
 	return front_ray.is_colliding()
 
-func _avoid_direction(current_dir: Vector3) -> Vector3:
-	if not left_ray.is_colliding():
+# This entire method is the reason I'm abandoning this NPC
+# I've tried for hours to get it to not get stuck in corners and failed miserably
+# Maybe I'm just bad at this but nothing I do seems to work
+# To whoever tries to improve this, I wish you the best of luck o7
+func _avoid_direction(_current_dir: Vector3) -> Vector3:
+	if !left_ray.is_colliding():
 		return -transform.basis.x
-	if not right_ray.is_colliding():
+	if !right_ray.is_colliding():
 		return transform.basis.x
-	return -current_dir
+	
+	if (right_ray.get_collision_point().distance_to(current_goal.global_position) <
+		left_ray.get_collision_point().distance_to(current_goal.global_position)):
+		return transform.basis.x
+	else:
+		return -transform.basis.x
 
 func _smooth_look(dir: Vector3, delta: float):
 	if dir.length() < 0.01:
 		return
-
 	var target_yaw = atan2(dir.x, dir.z)
-	var speed_mod = clamp(Vector3(velocity.x, 0, velocity.z).length() / chase_speed, 0.3, 1.0)
+	var speed_mod = clamp(Vector3(velocity.x, 0, velocity.z).length() / speed, 0.3, 1.0)
 	rotation.y = lerp_angle(rotation.y, target_yaw, turn_speed * speed_mod * delta)
 
-# ================== KÜLSŐ HÍVÁS ==================
+func calculate_target_path():
+	if !astar_map:
+		current_goal = target
+		return
+	
+	# Make the goal the target if the target is within view
+	line_of_sight.look_at(target.global_position)
+	line_of_sight.force_raycast_update()
+	if line_of_sight.get_collider() == target:
+		current_goal = target
+	elif current_goal == target:
+		# Clear the current goal if target leaves sightline
+		target_closest_node = null
+		current_goal = null 
+	
+	# Create a new goal if you don't have one
+	if !current_goal:
+		current_goal = instance_from_id(astar_map.astar.get_closest_point(self.global_position))
+	
+	# Only update path if it needs to be updated
+	var new_target_closest = instance_from_id(astar_map.astar.get_closest_point(target.global_position))
+	if new_target_closest == target_closest_node:
+		return
+	else:
+		target_closest_node = new_target_closest
+	
+	if !(current_goal is NavigationNode):
+		return
+	
+	# Update the path
+	target_path = astar_map.astar.get_id_path(current_goal.get_instance_id(), target_closest_node.get_instance_id())
+		
+	if target_path.size() <= 0:
+		return
+	
+	# Find the index of whatever the current goal is
+	var current_goal_index = target_path.find(current_goal.get_instance_id())
+	if current_goal_index > 0:
+		goal_index = current_goal_index
+	else: 
+		current_goal = instance_from_id(target_path[0])
+		goal_index = 0
+	
+	# Start heading towards the next goal if you're already on your way
+	if goal_index >= target_path.size() - 1:
+		return
+	var next_goal = instance_from_id(target_path[goal_index + 1]).global_position
+	if current_goal.global_position == next_goal && goal_index < target_path.size() - 2:
+		goal_index += 1
+		current_goal = instance_from_id(target_path[goal_index])
+		next_goal = instance_from_id(target_path[goal_index + 1]).global_position
+	
+	if (current_goal.global_position.distance_to(next_goal) > self.global_position.distance_to(next_goal)):
+			goal_index += 1
+			current_goal = instance_from_id(target_path[goal_index])
+
+# ================== EXTERNAL CALLS ==================
 
 func set_target(node: Node3D):
 	target = node
 
-
-func _on_hp_label_fade_area_body_entered(body: Node3D) -> void:
-	if body.is_in_group("Player"):
-		$Health3D/AnimationPlayer.play("in")
-		pass
-		
-func _on_hp_label_fade_area_body_exited(body: Node3D) -> void:
-	if body.is_in_group("Player"):
-		$Health3D/AnimationPlayer.play("out")
